a085f0c87a46b700c40fabe697ce990849bdeef6
i <97240499+kkanyu@users.noreply.github.com>
2022-10-17 13:28:09 -0400

    Upload project

--- react_hhs/src/App.js
+++ react_hhs/src/App.js
@@ -1,33 +1,17 @@
-// Kaylene-Nhu Nguyen @ Mohawk College, 2022
-import React, {useState} from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home.js';
-import { Personal } from './pages/Personal';
-import { Summary } from './pages/Summary';
-import { Error } from './pages/Error';
+import React, {useState} from "react"
+import ToDoList from "./ToDoList"
 
-export default function App() {
-
-  // Defaults for managing the multi-step form.
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    dob: null,
-    hcn: 0,
-    gender: "M"
-  });
-
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home formData={formData} setFormData={setFormData} />} />
-          <Route path="/personal" element={<Personal formData={formData} setFormData={setFormData} />} />
-          <Route path="/summary" element={<Summary formData={formData} setFormData={setFormData} />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+function App() {
+    const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+    return (
+        <>
+        <ToDoList todos={todos} />
+        <input type="text" />
+        <button>Add Item</button>
+        <button>Clear Items</button>
+        <div>0 left to do</div>
+        </>
+    )
 }
+
+export default App;
