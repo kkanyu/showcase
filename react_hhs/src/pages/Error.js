dc6c0e7a75f0954cda788fefc48a4d6fec827aa7
i <97240499+kkanyu@users.noreply.github.com>
2022-10-17 12:43:48 -0400

    Add files via upload

---
+++ react_hhs/src/pages/Error.js
@@ -0,0 +1,13 @@
+// Kaylene-Nhu Nguyen @ Mohawk College, 2022
+import React from 'react'
+
+export const Error = () => {
+  return (
+    <>
+    <div className="error">
+      <h1>Sorry, that path does not exist!</h1>
+      <a href="/">Go Back Home</a>
+    </div>
+    </>
+  )
+}
